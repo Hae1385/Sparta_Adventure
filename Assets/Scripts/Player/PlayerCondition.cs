ea1995ac8bcf73fb80a9a3c94b771a1f2378cded
Hae1385 <zx3248@naver.com>
2025-05-20 11:38:49 +0900

    Add : 체력과 스테미나 추가

---
+++ Assets/Scripts/Player/PlayerCondition.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public interface IDamagelbe
+{
+    void TakePhysiclaDamage(int damage);
+}
+public class PlayerCondition : MonoBehaviour, IDamagelbe
+{
+    public UICondition uiCondition;
+
+    Condition health { get { return uiCondition.health; } }
+    Condition stamina { get { return uiCondition.health; } }
+
+    public event Action onTakeDamage;
+    private void Update()
+    {
+        health.Add(health.passiveValue * Time.deltaTime);
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+    }
+
+    public void Heal(float amout)
+    {
+        health.Add(amout);
+    }
+
+    public void Die()
+    {
+        Debug.Log("die");
+    }
+
+    public void TakePhysiclaDamage(int damage)
+    {
+        health.Subject(damage);
+        onTakeDamage?.Invoke();
+    }
+
+    public bool UseStamina(float amount)
+    {
+        if (stamina.curValue - amount < 0f)
+        {
+            return false;
+        }
+        stamina.Subject(amount);
+        return true;
+    }
+
+}
