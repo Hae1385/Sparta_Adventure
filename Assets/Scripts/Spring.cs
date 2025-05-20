af66d42aed3caf9db48a7b54f517c48d4555069e
Hae1385 <zx3248@naver.com>
2025-05-20 12:38:56 +0900

    Add : 스프링 추가
    끼얏호~!

---
+++ Assets/Scripts/Spring.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+    public GameObject spring;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForce(Vector3.up * 500f, ForceMode.Impulse);
+        }
+    }
+
+}
